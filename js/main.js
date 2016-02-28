0f6d3bf17bb309be541793944b969201110cfb84
Galen <galen.scovell@gmail.com>
2016-02-28 14:32:11 -0800

    Updating plots with animations/transitions

--- js/main.js
+++ js/main.js
@@ -1,26 +1,5 @@
 
 
-var barchartPresent = false;
-var scatterplotPresent = false;
-
-
-$("#update-barchart").on("click", function(e) {
-    if (barchartPresent) {
-        d3.select("#barchart-svg").remove();
-    }
-    buildBarchart();
-    barchartPresent = true;
-});
-
-
-$("#update-scatterplot").on("click", function(e) {
-    if (scatterplotPresent) {
-        d3.select("#scatter-svg").remove();
-    }
-    buildScatterplot();
-    scatterplotPresent = true;
-});
-
 
 function pullDataFromCsv(fname) {
     loadedData = d3.csv(fname, function(data) {
@@ -32,8 +11,8 @@ function pullDataFromCsv(fname) {
 function createRandomData() {
     var dataset = [];
     var numPoints = 50;
-    var xRange = Math.random() * 1000;
-    var yRange = Math.random() * 1000;
+    var xRange = Math.random() * 200;
+    var yRange = Math.random() * 200;
 
     for (var i = 0; i < numPoints; i++) {
         var x = Math.floor(Math.random() * xRange);
@@ -50,9 +29,19 @@ function buildBarchart() {
     var containerHeight = $("#barchart").height();
     var barPadding = 2;
     
+    // Construct dataset
     var dataset = [ 5, 10, 13, 19, 21, 25, 22, 18, 15, 13,
                    11, 12, 15, 20, 18, 17, 16, 18, 23, 25];
 
+    // Construct scales
+    var xScale = d3.scale.ordinal()
+                            .domain(d3.range(dataset.length))
+                            .rangeRoundBands([0, containerWidth], 0.05);
+    var yScale = d3.scale.linear()
+                            .domain([0, d3.max(dataset)])
+                            .range([0, containerHeight]);
+
+    // Construct elements
     var svg = d3.select("div#barchart").append("svg")
                 .attr("id", "barchart-svg")
                 .attr("width", containerWidth)
@@ -87,13 +76,62 @@ function buildBarchart() {
         .attr("font-family", "Roboto")
         .attr("font-size", "14px")
         .attr("fill", "#f3f3f3")
+
+
+    // Update barchart on button click
+    d3.select("#update-barchart").on("click", function() {
+        var numValues = dataset.length;
+        dataset = [];
+
+        for (var i = 0; i < numValues; i++) {
+            var newNum = Math.floor(Math.random() * 25);
+            dataset.push(newNum);
+        }
+
+        yScale.domain([0, d3.max(dataset)]);
+
+        // Update all rects
+        svg.selectAll("rect")
+            .data(dataset)
+            .transition()
+            .delay(function(dval, idx) {
+                return idx / dataset.length * 600;
+            })
+            .duration(300)
+            .attr("y", function(dval) {
+                return containerHeight - yScale(dval);
+            })
+            .attr("height", function(dval) {
+                return yScale(dval);
+            })
+            .attr("fill", function(dval) {
+                return "rgb(0, 120, " + (dval * 10) + ")";
+            });
+
+        svg.selectAll("text")
+            .data(dataset)
+            .transition()
+            .delay(function(dval, idx) {
+                return idx / dataset.length * 600;
+            })
+            .duration(300)
+            .text(function(dval) {
+                return dval;
+            })
+            .attr("x", function(dval, idx) {
+                return idx * (containerWidth / dataset.length) + barPadding * 9;
+            })
+            .attr("y", function(dval) {
+                return containerHeight - yScale(dval) + 20;
+            });
+    });
 }
 
 
 function buildScatterplot() {
     var containerWidth = $("#scatterplot").width();
     var containerHeight = $("#scatterplot").height();
-    var padding = 40;
+    var padding = 30;
 
     // Create dataset
     var dataset = createRandomData();
@@ -119,12 +157,12 @@ function buildScatterplot() {
                         .orient("left")
                         .ticks(5);
 
+    // Create elements
     var svg = d3.select("div#scatterplot").append("svg")
                 .attr("id", "scatter-svg")
                 .attr("width", containerWidth)
                 .attr("height", containerHeight);
 
-    // Create elements
     svg.selectAll("circle").data(dataset).enter().append("circle")
         .attr("cx", function(dval) {
             return xScale(dval[0]);
@@ -132,25 +170,72 @@ function buildScatterplot() {
         .attr("cy", function(dval) {
             return yScale(dval[1]);
         })
-        .attr("r", function(dval) {
-            return rScale(dval[1]);
-        })
-        .attr("fill", function(dval) {
-            return "rgba(0, 100, 100, " + (dval[1] / 100) + ")";
-        });
+        .attr("r", 5)
+        .attr("fill", "rgb(26, 188, 156)");
 
     svg.append("g")
-        .attr("class", "axis")
+        .attr("class", "x axis")
         .attr("transform", "translate(0, " + (containerHeight - padding) + ")")
         .call(xAxis);
 
     svg.append("g")
-        .attr("class", "axis")
+        .attr("class", "y axis")
         .attr("transform", "translate(" + padding + ", 0)")
         .call(yAxis);
+
+
+    // Update scatterplot on button click
+    d3.select("#update-scatterplot").on("click", function() {
+        var numValues = dataset.length;
+        var maxRange = Math.random() * 200;
+        dataset = [];
+        for (var i = 0; i < numValues; i++) {
+            var x = Math.floor(Math.random() * maxRange);
+            var y = Math.floor(Math.random() * maxRange);
+            dataset.push([x, y]);
+        }
+
+        // Update scale domains
+        xScale.domain([0, d3.max(dataset, function(d) { return d[0]; })]);
+        yScale.domain([0, d3.max(dataset, function(d) { return d[1]; })]);
+
+        // Update all circles
+        svg.selectAll("circle")
+            .data(dataset)
+            .transition()
+            .duration(300)
+            .each("start", function() {
+                d3.select(this)
+                    .attr("fill", "rgb(231, 76, 60)")
+                    .attr("r", 2);
+            })
+            .attr("cx", function(dval) {
+                return xScale(dval[0]);
+            })
+            .attr("cy", function(dval) {
+                return yScale(dval[1]);
+            })
+            .transition()
+            .duration(400)
+            .attr("r", 5)
+            .attr("fill", "rgb(26, 188, 156)");
+
+        //Update X axis
+        svg.select(".x.axis")
+            .transition()
+            .duration(400)
+            .call(xAxis);
+        
+        //Update Y axis
+        svg.select(".y.axis")
+            .transition()
+            .duration(400)
+            .call(yAxis);
+    });
 }
 
 
 $(document).ready(function() {
-    
+    buildBarchart();
+    buildScatterplot();
 });
